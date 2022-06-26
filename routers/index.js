a3a0d73e017b581cdea08c1ad7bd404776a1f8c0
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-06-27 05:25:53 +0700

    update lagi oy

--- routers/index.js
+++ routers/index.js
@@ -23,7 +23,7 @@ const authentication = (req, res, next) => {
 };
 
 module.exports = (app) => {
-  app.get('/', (req, res) => res.send('UPDATE PENDING ROOM LAGII'));
+  app.get('/', (req, res) => res.send('Update'));
 
   app.post('/api/v1/payment', authentication, wrap(payment.create));
 
