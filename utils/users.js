38e971412aa98e47850f36d04aa7af427ec06f32
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-06-28 07:06:52 +0700

    update user utils

--- utils/users.js
+++ utils/users.js
@@ -4,16 +4,33 @@ exports.save = async (userId, user, FCMToken) => {
   try {
     const firestore = getFirestore();
 
-    await firestore
-      .collection('users')
-      .doc(userId)
-      .set({
-        name: user.displayName,
-        email: user.email,
-        avatar: user.photoURL,
-        status: 'Online',
-        FCMTokens: FieldValue.arrayUnion(FCMToken)
-      });
+    const userRef = firestore.collection('users').doc(userId);
+
+    const isUserExist = (await userRef.get()).exists;
+
+    if (!isUserExist) {
+      await firestore
+        .collection('users')
+        .doc(userId)
+        .set({
+          name: user.displayName,
+          email: user.email,
+          avatar: user.photoURL,
+          status: 'Online',
+          FCMTokens: [FCMToken]
+        });
+    } else {
+      await firestore
+        .collection('users')
+        .doc(userId)
+        .update({
+          name: user.displayName,
+          email: user.email,
+          avatar: user.photoURL,
+          status: 'Online',
+          FCMTokens: FieldValue.arrayUnion(FCMToken)
+        });
+    }
 
     return true;
   } catch (error) {
