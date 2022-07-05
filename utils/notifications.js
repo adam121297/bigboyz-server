8ca84771a6396a9f75b967eadcb3a3f0af8699f6
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-07-05 19:23:10 +0700

    123

--- utils/notifications.js
+++ utils/notifications.js
@@ -1,22 +1,56 @@
-const { getFirestore } = require('firebase-admin/firestore');
+const { getFirestore, FieldValue } = require('firebase-admin/firestore');
 const { getMessaging } = require('firebase-admin/messaging');
+
+const removeInvalidToken = async (userId, FCMTokens) => {
+  try {
+    const firestore = getFirestore();
+
+    const userRef = firestore.collection('users').doc(userId);
+
+    FCMTokens.forEach((token) =>
+      userRef.update({ FCMTokens: FieldValue.arrayRemove(token) })
+    );
+  } catch (error) {
+    return { error };
+  }
+};
 
 exports.send = async (userId, messageTitle, messageBody) => {
   try {
     const firestore = getFirestore();
     const messaging = getMessaging();
 
-    const rawData = await firestore.collection('users').doc(userId).get();
-    const user = rawData.data();
+    const userRef = firestore.collection('users').doc(userId);
+    const user = (await userRef.get()).data();
 
     if (user) {
-      await messaging.sendMulticast({
-        tokens: user.FCMTokens,
-        notification: {
-          body: messageBody,
-          title: messageTitle
+      const response = await messaging.sendToDevice(
+        user.FCMTokens,
+        {
+          notification: {
+            body: messageBody,
+            title: messageTitle
+          }
+        },
+        { priority: 'high' }
+      );
+
+      const FCMTokens = user.FCMTokens;
+      const invalidFCMTokens = [];
+
+      response.results.forEach((result, index) => {
+        const error = result.error;
+        if (error) {
+          if (
+            error.code === 'messaging/invalid-registration-token' ||
+            error.code === 'messaging/registration-token-not-registered'
+          ) {
+            invalidFCMTokens.push(FCMTokens[index]);
+          }
         }
       });
+
+      removeInvalidToken(userId, invalidFCMTokens);
     }
 
     return true;
