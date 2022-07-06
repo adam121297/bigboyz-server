c52eb642339c306cbb6031d779bd1ecfd2a2fa55
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-07-06 16:12:12 +0700

    message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
-const { getFirestore } = require('firebase-admin/firestore');
+const { getFirestore, FieldValue } = require('firebase-admin/firestore');
 
 const notifications = require('./utils/notifications');
 const midtrans = require('./utils/midtrans');
@@ -24,6 +24,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening to port ${port}...`));
 
 const firestore = getFirestore();
+
 firestore.collection('notifications').onSnapshot((snapshot) => {
   if (snapshot.empty) {
     return;
@@ -31,7 +32,21 @@ firestore.collection('notifications').onSnapshot((snapshot) => {
 
   snapshot.docChanges().forEach((change) => {
     if (change.type === 'added') {
-      const { id, receiver, message } = change.doc.data();
+      const { id, sender, receiver, message } = change.doc.data();
+
+      firestore
+        .collection('users')
+        .doc(receiver)
+        .collection('chatRooms')
+        .doc(id)
+        .update({
+          latestMessage: {
+            text: message.text,
+            sender,
+            timestamp: message.timestamp
+          },
+          counter: FieldValue.increment(1)
+        });
 
       notifications.send(receiver, {
         id,
