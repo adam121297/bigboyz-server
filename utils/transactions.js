86d47d35ee467e5d5a15bbff64ca7a5bb1de097e
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-12-27 12:25:51 +0700

    update utils

--- utils/transactions.js
+++ utils/transactions.js
@@ -1,39 +1,4 @@
-const {
-  getMonth,
-  getYear,
-  format,
-  getTime,
-  endOfYear,
-  startOfYear
-} = require('date-fns');
 const { getFirestore } = require('firebase-admin/firestore');
-const { id } = require('date-fns/locale');
-
-const groupByMonth = (array) => {
-  const groupedData = {};
-
-  array.forEach((data) => {
-    const createdAt = data.payment.createdAt;
-    const month = getMonth(createdAt);
-    const year = getYear(createdAt);
-
-    const firstDayOfMonth = new Date(year, month, 1, 0, 0, 0);
-    const key = getTime(firstDayOfMonth);
-
-    if (year === getYear(Date.now())) {
-      if (!groupedData[key]) groupedData[key] = [];
-      groupedData[key].push(data);
-    }
-  });
-
-  return Object.keys(groupedData).map((key) => {
-    return {
-      timestamp: key,
-      month: format(Number(key), 'MMMM', { locale: id }),
-      data: groupedData[key]
-    };
-  });
-};
 
 /**
  * Create transaction data
@@ -71,35 +36,6 @@ exports.update = async (transactionId, status) => {
     });
 
     return true;
-  } catch (error) {
-    return { error };
-  }
-};
-
-exports.getCurrentYear = async (grouped = false) => {
-  try {
-    const firestore = getFirestore();
-
-    const currentTimestamp = Date.now();
-
-    const timestampStartOfYear = getTime(startOfYear(currentTimestamp));
-    const timestampEndOfYear = getTime(endOfYear(currentTimestamp));
-
-    const transactionsData = await firestore
-      .collection('transactions')
-      .where('payment.createdAt', '>=', timestampStartOfYear)
-      .where('payment.createdAt', '<=', timestampEndOfYear)
-      .get();
-
-    if (grouped) {
-      return transactionsData.empty
-        ? []
-        : groupByMonth(transactionsData.docs.map((data) => data.data()));
-    } else {
-      return transactionsData.empty
-        ? []
-        : transactionsData.docs.map((data) => data.data());
-    }
   } catch (error) {
     return { error };
   }
