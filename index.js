90a8d5a181ce274a1750fc694e31c31d52ba8b41
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-12-27 15:12:33 +0700

    add cron

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
+const cron = require('node-cron');
+const { check } = require('./utils/orders');
+
+// CRON every night
+cron.schedule('0 0 * * *', () => {
+  check()
+    .then((status) => {
+      console.log(status);
+    })
+    .catch((error) => {
+      return { error };
+    });
+});
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
